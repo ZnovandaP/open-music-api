b76c7d83943c079634347b70e74df5bf9f6748e1
ZnovandaP <znovandaputra@gmail.com>
2024-09-13 01:33:46 +0700

    feat(migrations): add new file migrations for create table playlist and playlist_songs

---
+++ migrations/1726111529429_create-table-playlist-and-playlist-song.js
@@ -0,0 +1,59 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('playlists', {
+    id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users(id)',
+      onDelete: 'cascade',
+    },
+  });
+
+  pgm.createTable('playlist_songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'playlists(id)',
+      onDelete: 'cascade',
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'songs(id)',
+      onDelete: 'cascade',
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('playlist');
+  pgm.dropTable('playlist_songs');
+};
