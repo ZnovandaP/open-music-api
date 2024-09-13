b49f8a621e62644bb49d945690c1ac71c7748f43
ZnovandaP <znovandaputra@gmail.com>
2024-09-13 15:41:34 +0700

    fix(plugin->handler:playlist) change method getPlaylistById to getPlaylistByOwnerId

--- src/api/playlists/handler.js
+++ src/api/playlists/handler.js
@@ -36,7 +36,7 @@ class PlaylistsHandler {
   async getPlaylistsHandler(request, h) {
     const { id: ownerId } = request.auth.credentials;
 
-    const playlists = await this._playlistsService.getPlaylistById(ownerId);
+    const playlists = await this._playlistsService.getPlaylistsByOwnerId(ownerId);
 
     return {
       status: 'success',
